449cbc68fd8f70e12eff7052cd012c57c2ee83be
Artem Semidolin <art.semidolin2011@gmail.com>
2020-11-26 23:31:03 +0300

    Modified  get_ast.py to match PEP8. Add test.ast in .gitignore.

--- get_ast.py
+++ get_ast.py
@@ -1,21 +1,21 @@
-from clang.cindex import *
 import os
 import sys
+from clang.cindex import CursorKind, Index, TranslationUnit
+# from clang.cindex import *
 
-    
-IGNORE = [ CursorKind.PREPROCESSING_DIRECTIVE, 
-    CursorKind.MACRO_DEFINITION, 
-    CursorKind.MACRO_INSTANTIATION, 
-    CursorKind.INCLUSION_DIRECTIVE, 
-    CursorKind.USING_DIRECTIVE,
-    CursorKind.NAMESPACE]
+IGNORE = [CursorKind.PREPROCESSING_DIRECTIVE,
+          CursorKind.MACRO_DEFINITION,
+          CursorKind.MACRO_INSTANTIATION,
+          CursorKind.INCLUSION_DIRECTIVE,
+          CursorKind.USING_DIRECTIVE,
+          CursorKind.NAMESPACE]
 
 
 def get_cursor_from_file(filename, args=[]):
     '''
         Returns clang.cindex.Cursor object or 0 if file is undefined
         @param filename - fuul path to source file
-        @param args - list of arguments for clang.cindex.Index.parse() method  
+        @param args - list of arguments for clang.cindex.Index.parse() method
     '''
     if not os.path.isfile(filename):
         print(filename, "Is not a file / doesn't exist")
@@ -23,26 +23,29 @@ def get_cursor_from_file(filename, args=[]):
 
     index = Index.create()
     options = TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD
-    
+
     file_obj = index.parse(filename, args=args, options=options) or 0
     return file_obj.cursor
 
 
-def traverse_and_print_values(cursor, src, output_path = './tree.ast', depth = 0):
+def traverse_and_print_values(cursor, src, output_path='./tree.ast', depth=0):
     '''
         Traverses and make file with AST of input file
-        @param cursor - clang.cindex.Cursor object (look at get_cursor_from_file() )
-        @param src - path to source .c or .cpp file 
-        @param output_path - full path to the output file (with name and extension)
+        @param cursor - clang.cindex.Cursor object
+        (look at get_cursor_from_file() )
+        @param src - path to source .c or .cpp file
+        @param output_path - full path to the output file
+        (with name and extension)
         @param depth - level of depth in AST tree (influences indentation)
     '''
     children = cursor.get_children()
     for child in children:
-        if str(child.location.file).split('/')[-1] == src.split('/')[-1] and child.kind not in IGNORE:
+        if (str(child.location.file).split('/')[-1] == src.split('/')[-1]
+           and child.kind not in IGNORE):
             output = (str('|  ' * depth) +
-                str(child.kind) + " '" + 
-                str(child.spelling) + "' " + 
-                str(len(list(child.get_children()))) + '\n')
+                      str(child.kind) + " '" +
+                      str(child.spelling) + "' " + '\n')
+            # str(len(list(child.get_children()))) + '\n')
             # print(output)
 
             with open(output_path, 'a') as output_file:
@@ -57,7 +60,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         filename = sys.argv[1]
-    
+
     cursor = get_cursor_from_file(filename)
     if cursor:
         if len(sys.argv) > 2:
@@ -69,4 +72,3 @@ if __name__ == '__main__':
             os.remove(output_path)
 
         traverse_and_print_values(cursor, filename, output_path)
-    
