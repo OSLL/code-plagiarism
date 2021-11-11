18c9f66473116bdb80e596bb4ef5ba07628eae67
Artanias <43622365+Artanias@users.noreply.github.com>
2021-11-11 14:40:13 +0300

    Added tests for parse_content_url function

--- src/webparsers/tests/test_github_parser.py
+++ src/webparsers/tests/test_github_parser.py
@@ -1,19 +1,24 @@
 import unittest
-from unittest.mock import patch, Mock, call
 
 from webparsers.github_parser import GitHubParser
+
 
 class TestGitHubParser(unittest.TestCase):
 
     def test_check_github_url(self):
         test_cases = [
             {
-                'arguments': {"github_url" : "https://github.com/index"},
-                'expected_result': ["https:", "", "github.com", "index"],
+                'arguments': {
+                    "github_url": "https://github.com/OSLL"
+                },
+                'expected_result': ["https:", "", "github.com", "OSLL"],
             },
             {
-                'arguments': {"github_url" : "http://github.com/OSLL/test.py"},
-                'expected_result': ["http:", "", "github.com", "OSLL", "test.py"],
+                'arguments': {
+                    "github_url": "http://github.com/OSLL/code-plagiarism/"
+                },
+                'expected_result': ["http:", "", "github.com",
+                                    "OSLL", "code-plagiarism"],
             }
         ]
 
@@ -25,13 +30,19 @@ class TestGitHubParser(unittest.TestCase):
     def test_check_github_url_bad(self):
         test_cases = [
             {
-                'arguments': {"github_url" : "ttps://github.com/index"},
+                'arguments': {
+                    "github_url": "ttps://github.com/OSLL"
+                },
             },
             {
-                'arguments': {"github_url" : "http:/j/github.com/OSLL/test.py"},
+                'arguments': {
+                    "github_url": "https:/j/github.com/OSLL"
+                },
             },
             {
-                'arguments': {"github_url" : "http://githUb.com/OSLL/test.py"},
+                'arguments': {
+                    "github_url": "https://githUb.com/OSLL"
+                },
             }
         ]
 
@@ -43,11 +54,15 @@ class TestGitHubParser(unittest.TestCase):
     def test_parse_repo_url(self):
         test_cases = [
             {
-                'arguments': {"repo_url" : "https://github.com/OSLL/code-plagiarism"},
+                'arguments': {
+                    "repo_url": "https://github.com/OSLL/code-plagiarism"
+                },
                 'expected_result': ("OSLL", "code-plagiarism"),
             },
             {
-                'arguments': {"repo_url" : "http://github.com/OSLL/test.py"},
+                'arguments': {
+                    "repo_url": "http://github.com/OSLL/test.py"
+                },
                 'expected_result': ("OSLL", "test.py"),
             }
         ]
@@ -60,16 +75,22 @@ class TestGitHubParser(unittest.TestCase):
     def test_parse_repo_url_bad(self):
         test_cases = [
             {
-                'arguments': {"repo_url" : "ttps://github.com/index"},
+                'arguments': {
+                    "repo_url": "ttps://github.com/index"
+                },
             },
             {
-                'arguments': {"repo_url" : "http:/j/github.com/OSLL/test.py"},
+                'arguments': {
+                    "repo_url": "http:/j/github.com/OSLL/test.py"
+                },
             },
             {
-                'arguments': {"repo_url" : "http://githUb.com/OSLL/test.py"},
+                'arguments': {
+                    "repo_url": "http://githUb.com/OSLL/test.py"
+                },
             },
             {
-                'arguments': {"repo_url" : "http://github.com/OSLL"},
+                'arguments': {"repo_url": "http://github.com/OSLL"},
             }
         ]
 
@@ -77,6 +98,61 @@ class TestGitHubParser(unittest.TestCase):
             with self.subTest(test_case=test_case):
                 with self.assertRaises(ValueError):
                     GitHubParser.parse_repo_url(**test_case['arguments'])
+
+    def test_parse_content_url(self):
+        test_cases = [
+            {
+                'arguments': {
+                    "content_url": "https://github.com/OSLL/code-plagiarism/blob/main/src/codeplag/astfeatures.py"
+                },
+                'expected_result': ('OSLL', 'code-plagiarism', 'main', 'src/codeplag/astfeatures.py'),
+            },
+            {
+                'arguments': {
+                    "content_url": "https://github.com/OSLL/code-plagiarism/blob/main/src/codeplag/logger.py"
+                },
+                'expected_result': ('OSLL', 'code-plagiarism', 'main', 'src/codeplag/logger.py'),
+            }
+        ]
+
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                result = GitHubParser.parse_content_url(**test_case['arguments'])
+                self.assertEqual(result, test_case['expected_result'])
+
+    def test_parse_content_url_bad(self):
+        test_cases = [
+            {
+                'arguments': {
+                    "content_url": "ttps://github.com/index"
+                },
+            },
+            {
+                'arguments': {
+                    "content_url": "http:/j/github.com/OSLL/test.py"
+                },
+            },
+            {
+                'arguments': {
+                    "content_url": "http://githUb.com/OSLL/test.py"
+                },
+            },
+            {
+                'arguments': {
+                    "content_url": "http://github.com/OSLL"
+                },
+            },
+            {
+                'arguments': {
+                    "content_url": "http://github.com/OSLL/test/tmp"
+                },
+            }
+        ]
+
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                with self.assertRaises(ValueError):
+                    GitHubParser.parse_content_url(**test_case['arguments'])
 
 
 if __name__ == '__main__':
