45dd73e8354e625c6fb94005877b0c1cfd6ea9a6
Artanias <43622365+Artanias@users.noreply.github.com>
2021-11-19 12:06:31 +0300

    Added tests for send_get_request function

--- src/webparsers/tests/test_github_parser.py
+++ src/webparsers/tests/test_github_parser.py
@@ -1,5 +1,6 @@
 import unittest
 
+from unittest.mock import patch
 from webparsers.github_parser import GitHubParser
 
 
@@ -204,6 +205,138 @@ class TestGitHubParser(unittest.TestCase):
             with self.subTest(test_case=test_case):
                 rv = test_case['parser'].is_accepted_extension(**test_case['arguments'])
                 self.assertEqual(rv, test_case['expected_result'])
+
+    @patch('webparsers.github_parser.requests.get')
+    def test_send_get_request(self, mock_get):
+        class Response:
+            def __init__(self, status_code, message=None):
+                self.status_code = status_code
+
+            def raise_for_status(self):
+                return None
+
+        test_cases = [
+            {
+                'arguments': {
+                    'api_url': 'users/moevm/repos',
+                    'params': {}
+                },
+                'get_arguments': [
+
+                ],
+                'get_posargs': ['https://api.github.com/users/moevm/repos'],
+                'get_kwargs': {
+                    'headers': {'accept': 'application/vnd.github.v3+json'},
+                    'params': {}
+                },
+                'response': Response(200)
+            }
+        ]
+
+        parser = GitHubParser()
+        for test_case in test_cases:
+            mock_get.reset_mock()
+            mock_get.return_value = test_case['response']
+
+            with self.subTest(test_case=test_case):
+                rv = parser.send_get_request(**test_case['arguments'])
+                self.assertEqual(rv, test_case['response'])
+
+                mock_get.assert_called_once_with(*test_case['get_posargs'],
+                                                 **test_case['get_kwargs'])
+
+    @patch('webparsers.github_parser.requests.get')
+    def test_send_get_request_bad(self, mock_get):
+        class Response:
+            def __init__(self, status_code, message=None):
+                self.status_code = status_code
+                self.message = message
+
+            def json(self):
+                return {'message': self.message} if self.message else {}
+
+        test_cases = [
+            {
+                'arguments': {
+                    'api_url': 'Test/url',
+                    'params': {}
+                },
+                'get_arguments': [
+
+                ],
+                'get_posargs': ['https://api.github.com/Test/url'],
+                'get_kwargs': {
+                    'headers': {'accept': 'application/vnd.github.v3+json'},
+                    'params': {}
+                },
+                'response': Response(403, "Not Found"),
+                'raised': Exception
+            },
+            {
+                'arguments': {
+                    'api_url': 'bad/link',
+                    'params': {}
+                },
+                'get_posargs': ['https://api.github.com/bad/link'],
+                'get_kwargs': {
+                    'headers': {'accept': 'application/vnd.github.v3+json'},
+                    'params': {}
+                },
+                'response': Response(403),
+                'raised': KeyError
+            },
+            {
+                'arguments': {
+                    'api_url': 'bad/link',
+                    'params': {
+                        'per_page': 100,
+                        'page': 5
+                    }
+                },
+                'get_posargs': ['https://api.github.com/bad/link'],
+                'get_kwargs': {
+                    'headers': {'accept': 'application/vnd.github.v3+json'},
+                    'params': {
+                        'per_page': 100,
+                        'page': 5
+                    }
+                },
+                'response': Response(403),
+                'raised': KeyError
+            },
+            {
+                'arguments': {
+                    'api_url': 'bad/link',
+                    'params': {}
+                },
+                'token': 'test_token',
+                'get_posargs': ['https://api.github.com/bad/link'],
+                'get_kwargs': {
+                    'headers': {
+                        'accept': 'application/vnd.github.v3+json',
+                        'Authorization': 'token test_token'
+                    },
+                    'params': {}
+                },
+                'response': Response(403),
+                'raised': KeyError
+            },
+        ]
+
+        parser = GitHubParser()
+        for test_case in test_cases:
+            if test_case.get('token'):
+                parser = GitHubParser(access_token=test_case['token'])
+
+            mock_get.reset_mock()
+            mock_get.return_value = test_case['response']
+
+            with self.subTest(test_case=test_case):
+                with self.assertRaises(test_case['raised']):
+                    parser.send_get_request(**test_case['arguments'])
+
+                mock_get.assert_called_once_with(*test_case['get_posargs'],
+                                                 **test_case['get_kwargs'])
 
 
 if __name__ == '__main__':
