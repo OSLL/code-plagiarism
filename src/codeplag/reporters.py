fe47291085ff4cba7d645961a02ab9e924bc78b6
Daniil Lokosov <lokosov.danil@yandex.ru>
2025-03-24 20:34:33 +0300

    CACHE-3 - lint fixes

--- src/codeplag/reporters.py
+++ src/codeplag/reporters.py
@@ -21,8 +21,6 @@ from codeplag.types import (
 
 
 class AbstractReporter(ABC):
-    def __init__(self: Self) -> None: ...
-
     @abstractmethod
     def save_result(
         self: Self,
@@ -41,7 +39,6 @@ class AbstractReporter(ABC):
 
 class CSVReporter(AbstractReporter):
     def __init__(self: Self, reports: Path) -> None:
-        super().__init__()
         self.reports = reports
         self.reports_path = self.reports / CSV_REPORT_FILENAME
         self.__need_update: bool = False
