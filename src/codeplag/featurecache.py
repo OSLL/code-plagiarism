c077de6f40147d8606b67d2c92a61b37ea5d9ba7
Daniil Lokosov <lokosov.danil@yandex.ru>
2025-03-24 20:07:31 +0300

    CACHE-3 - add AbstractFeatureCache as ASTFeature save abstraction

---
+++ src/codeplag/featurecache.py
@@ -0,0 +1,14 @@
+from abc import abstractmethod, ABC
+
+from typing_extensions import Self
+
+from codeplag.types import ASTFeatures
+
+
+class AbstractFeatureCache(ABC):
+
+    @abstractmethod
+    def save_feature(self: Self, feature: ASTFeatures) -> None: ...
+
+    @abstractmethod
+    def get_feature(self: Self, work: ASTFeatures) -> ASTFeatures | None: ...
