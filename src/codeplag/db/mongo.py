1d72e7d38942a57c2c351e277dca190c3db7f6a5
vRedniy-LumbeRt <112636677+vRedniy-LumbeRt@users.noreply.github.com>
2025-03-25 19:59:45 +0300

    Uncommented serialisation method. Methods' comments corrected to fit PEP8. Deleted duplicate variable "path" in document in write_feature.

--- src/codeplag/db/mongo.py
+++ src/codeplag/db/mongo.py
@@ -8,6 +8,8 @@ from typing import Dict
 from typing_extensions import Self
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+from pymongo.collection import Collection
+from codeplag.reporters import serialize_compare_result_to_dict
 from codeplag.types import ASTFeatures, CompareInfo, FastMetrics, StructuresInfo
 from codeplag.logger import codeplag_logger as logger
 
@@ -20,15 +22,17 @@ PASSWORD = "example"
 
 
 class MongoDBConnection:
-    def __init__(self: Self, host: str = HOST, user: str = USER, password: str = PASSWORD, db_name: str = "new_database"):
-        """
-        Initialization of the connection to MongoDB.
+    def __init__(self: Self, host: str = HOST, user: str = USER,
+                 password: str = PASSWORD, db_name: str = "new_database") -> None:
+        """Initialize the connection to MongoDB.
 
-        :param host: MongoDB host.
-        :param user: MongoDB username.
-        :param password: MongoDB password.
-        :param db_name: Database name.
+        Args:
+            host (str): MongoDB host address.
+            user (str): MongoDB username for authentication.
+            password (str): MongoDB password for authentication.
+            db_name (str): Name of the database to connect to.
         """
+
         self.url = self.url = f"mongodb://{user}:{password}@{host}:27017/"
         self.db_name = db_name
         self.client = None
@@ -40,10 +44,13 @@ class MongoDBConnection:
         # Registering the disconnect method for execution upon program termination
         atexit.register(self.disconnect)
 
-    def connect(self: Self):
+    def connect(self: Self) -> None:
+        """Establish a connection to MongoDB.
+
+        Attempts to connect to the MongoDB server and logs the result.
+        Raises an exception if the connection fails.
         """
-        Connecting to MongoDB.
-        """
+
         try:
             self.client = MongoClient(self.url, serverSelectionTimeoutMS=5000)
             self.client.admin.command('ping')  # Checking the connection
@@ -53,30 +60,33 @@ class MongoDBConnection:
             logger.error(f"Failed to connect to MongoDB: {e}")
             raise
 
-    def disconnect(self: Self):
+    def disconnect(self: Self) -> None:
+        """Close the connection to MongoDB.
+
+        Ensures that the MongoDB client is properly closed upon program termination.
         """
-        Disconnecting from MongoDB.
-        """
+
         if self.client:
             self.client.close()
             logger.debug("MongoDB connection closed.")
 
-    def get_collection(self: Self, collection_name: str):
-        """
-        Getting a collection by name from the current database.
+    def get_collection(self: Self, collection_name: str) -> Collection:
+        """Get a collection by name from the current database.
 
-        :param collection_name: Name of the collection.
-        :return: Collection object of the current database.
+        Args:
+            collection_name (str): The name of the collection to retrieve.
+
+        Returns:
+            Collection: The MongoDB collection object.
         """
 
         return self.db[collection_name]
 
 
 class ReportRepository:
-    def __init__(self: Self, mongo_connection: MongoDBConnection):
-        """
-        Initialization of the repository for the compare_info collection.
-        """
+    def __init__(self: Self, mongo_connection: MongoDBConnection) -> None:
+        """Initialization of the repository for the compare_info collection."""
+
         self.collection = mongo_connection.get_collection("compare_info")
 
     def write_compare_info(
@@ -84,16 +94,17 @@ class ReportRepository:
             work1: ASTFeatures,
             work2: ASTFeatures,
             compare_info: CompareInfo
-    ):
-        """
-        Insert or update a document in the compare_info collection.
-        Primary key: _id (sorted paths).
+    ) -> None:
+        """Insert or update a document in the compare_info collection.
+
+        The primary key (_id) is formed as a dictionary with sorted file paths.
 
         Args:
             work1 (ASTFeatures): The first file for comparison.
             work2 (ASTFeatures): The second file for comparison.
-            compare_info (CompareInfo): Comparison information.
+            compare_info (CompareInfo): Information about the comparison results.
         """
+
         # Sorting paths to create a unique primary key
         work1, work2 = sorted([work1, work2])
         first_path, second_path = [str(work1.filepath), str(work2.filepath)]
@@ -102,7 +113,7 @@ class ReportRepository:
         document_id = {"first": first_path, "second": second_path}
 
         # Using the serialize_compare_result_to_dict function to convert data
-        serialized_compare_info = None  # serialize_compare_result_to_dict(compare_info)
+        serialized_compare_info = serialize_compare_result_to_dict(compare_info)
 
         document = {
             "_id": document_id,
@@ -125,30 +136,29 @@ class ReportRepository:
 
 
 class FeatureRepository:
-    def __init__(self: Self, mongo_connection: MongoDBConnection):
-        """
-        Initialization of the repository for the features collection.
-        """
+    def __init__(self: Self, mongo_connection: MongoDBConnection) -> None:
+        """Initialization of the repository for the features collection."""
+
         self.collection = mongo_connection.get_collection("features")
 
-    def write_feature(self: Self, work: ASTFeatures, cmp_for_feature: CompareInfo):
-        """
-        Insert or update a document in the features collection.
-        Primary key: path.
+    def write_feature(self: Self, work: ASTFeatures, cmp_for_feature: CompareInfo) -> None:
+        """Insert or update a document in the features collection.
+
+        The primary key (_id) is formed using the file path.
 
         Args:
-            work (ASTFeatures): File for saving features.
-            cmp_for_feature (CompareInfo): Object with required structure data.
+            work (ASTFeatures): The file for which features are being saved.
+            cmp_for_feature (CompareInfo): An object containing structural data for comparison.
         """
+
         # Forming _id as the file path
-        document_id = {"first": str(work.filepath)}
+        document_id = str(work.filepath)
 
         # Using the serialize_compare_result_to_dict function to convert data
-        serialized_compare_info = None  # serialize_compare_result_to_dict(cmp_for_feature)
+        serialized_compare_info = serialize_compare_result_to_dict(cmp_for_feature)
 
         document = {
             "_id": document_id,
-            "path": document_id,
             "modify_date": work.modify_date,
             "sha256": work.sha256,
             "features": {
