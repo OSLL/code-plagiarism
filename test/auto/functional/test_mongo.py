f0bcaaa7adf5cc123a8d1ec3e1f7bb781f8d31b7
Daniil Lokosov <lokosov.danil@yandex.ru>
2025-05-17 18:36:10 +0300

    CACHE-56 - first test iteration

---
+++ test/auto/functional/test_mongo.py
@@ -0,0 +1,76 @@
+import time
+
+import pytest
+from consts import CONFIG_PATH, DEFAULT_MONGO_PASS, DEFAULT_MONGO_USER
+from db.mongo import MongoDBConnection
+from testcontainers.mongodb import MongoDbContainer
+from utils import modify_settings, run_check
+
+PY_SIM_FILES = ["unit/data/test1.py", "unit/data/test2.py"]
+PY_FILES = ["unit/data/test1.py", "unit/data/test3.py"]
+
+
+@pytest.fixture(scope="module")
+def mongo_container() -> MongoDbContainer:
+    with MongoDbContainer(
+        "mongo:6.0", username=DEFAULT_MONGO_USER, password=DEFAULT_MONGO_PASS
+    ) as mongo:
+        mongo.start()
+        time.sleep(10)
+        yield mongo
+
+
+@pytest.fixture(scope="module")
+def mongo_connection(mongo_container: MongoDbContainer) -> MongoDBConnection:
+    host = mongo_container.get_container_host_ip()
+    port = int(mongo_container.get_exposed_port(27017))
+    user = mongo_container.username
+    password = mongo_container.password
+
+    conn = MongoDBConnection(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+    )
+    yield conn
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup_module(mongo_connection: MongoDBConnection) -> None:
+    modify_settings(
+        log_level="trace",
+        reports_extension="mongo",
+        mongo_host=mongo_connection.host,
+        mongo_port=mongo_connection.port,
+        mongo_user=mongo_connection.user,
+        mongo_pass=mongo_connection.password,
+    ).assert_success()
+
+    yield
+
+    CONFIG_PATH.write_text("{}")
+
+
+@pytest.fixture(autouse=True)
+def clear_db(mongo_connection: MongoDBConnection) -> None:
+    mongo_connection.clear_db()
+
+    yield
+
+
+@pytest.mark.parametrize(
+    "cmd, found_plag",
+    [
+        (["--files", *PY_FILES], False),
+        (["--files", *PY_SIM_FILES], True),
+    ],
+)
+def test_py_correct_mongo_connection(cmd: list[str], found_plag: bool):
+    result = run_check(cmd, extension="py")
+
+    if found_plag:
+        result.assert_found_similarity()
+    else:
+        result.assert_success()
+    assert "Successfully connected to MongoDB!" in result.cmd_res.stdout
