6382843e13e47f2337e5f5b411b1a072c23a58f8
Artanias <43622365+Artanias@users.noreply.github.com>
2025-11-08 00:47:02 +0300

    refactor: removes repeated dependencies from setup.py and use from pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import sys
+import tomllib
+
+with open("pyproject.toml", "rb") as f:
+    pyproject_content = tomllib.load(f)
 
 BUILD_REQUIREMENTS: tuple[str, ...] = (
     "argparse-manpage==4.6",
@@ -17,23 +21,6 @@ TEST_REQUIREMENTS: tuple[str, ...] = (
     "pytest-mock~=3.15.1",
     "pytest-cov~=7.0.0",
 )
-INSTALL_REQUIREMENTS: tuple[str, ...] = (
-    "argcomplete~=3.5.3",
-    "numpy~=1.26.4",
-    "pandas~=2.2.3",
-    "ccsyspath~=1.1.0",
-    "clang~=18.1.8",
-    "llvmlite~=0.45.1",
-    "libclang~=18.1.1",
-    "python-decouple~=3.8",
-    "requests~=2.32.3",
-    "typing-extensions~=4.12.2",
-    "aiohttp~=3.9.3",
-    "Jinja2~=3.1.5",
-    "cachetools==5.5.2",
-    "gidgethub~=5.3.0",
-    "pymongo~=4.12.1",
-)
 
 
 if "--build-requirements" in sys.argv:
@@ -46,7 +33,7 @@ if "--test-requirements" in sys.argv:
     print(" ".join(TEST_REQUIREMENTS))
     sys.exit(0)
 if "--install-requirements" in sys.argv:
-    print(" ".join(INSTALL_REQUIREMENTS))
+    print(" ".join(pyproject_content["project"]["dependencies"]))
     sys.exit(0)
 try:
     from Cython.Build import cythonize
