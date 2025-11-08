4ef5bc29b8377630c57995992d031c83daae6c9a
Artanias <43622365+Artanias@users.noreply.github.com>
2025-11-08 11:33:14 +0300

    refactor: moves dependencies into pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
 import sys
 import tomllib
+from typing import Final
 
 with open("pyproject.toml", "rb") as f:
     pyproject_content = tomllib.load(f)
 
-BUILD_REQUIREMENTS: tuple[str, ...] = (
-    "argparse-manpage==4.6",
-    "Babel==2.17.0",
-    "Cython~=3.0.12",
-    "setuptools~=75.8.1",
-    "Jinja2~=3.1.5",
+BUILD_REQUIREMENTS: Final = (
+    pyproject_content["build-system"]["requires"]
+    + pyproject_content["dependency-groups"]["translate"]
+    + pyproject_content["dependency-groups"]["man"]
 )
-# fmt: off
-LINT_REQUIREMENTS: tuple[str, ...] = (
-    "pre-commit~=4.3.0",
-)
-# fmt: on
-TEST_REQUIREMENTS: tuple[str, ...] = (
-    "pytest~=8.4.2",
-    "pytest-mock~=3.15.1",
-    "pytest-cov~=7.0.0",
-)
+LINT_REQUIREMENTS: Final = pyproject_content["dependency-groups"]["lint"]
+TEST_REQUIREMENTS: Final = pyproject_content["dependency-groups"]["test"]
+INSTALL_REQUIREMENTS: Final = pyproject_content["project"]["dependencies"]
 
 
 if "--build-requirements" in sys.argv:
@@ -33,7 +25,7 @@ if "--test-requirements" in sys.argv:
     print(" ".join(TEST_REQUIREMENTS))
     sys.exit(0)
 if "--install-requirements" in sys.argv:
-    print(" ".join(pyproject_content["project"]["dependencies"]))
+    print(" ".join(INSTALL_REQUIREMENTS))
     sys.exit(0)
 try:
     from Cython.Build import cythonize
